1f87e0a13a9e6d4cbaac57017daf57f9f0e5e988
travn1k <travn1k@inbox.ru>
2022-01-16 10:52:37 +0300

    Добавил NavLink для блокировки перезагрузки страницы

--- social-web-app/src/Components/Sidebar/Sidebar.jsx
+++ social-web-app/src/Components/Sidebar/Sidebar.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
 import './Sidebar.css'
+import {NavLink} from "react-router-dom";
 
 export default class Sidebar extends Component {
     render() {
         return (
             <div className='sidebar-wrapper'>
                 <nav className='nav-menu'>
-                    <a href='/feed'><button type="button" className='btn btn-outline-light btn-1'>Новости</button></a>
-                    <a href='/profile'><button type="button" className='btn btn-outline-light btn-2'>Профиль</button></a>
-                    <a href='/friends'><button type="button" className='btn btn-outline-light btn-6'>Друзья</button></a>
-                    <a href='/messages'><button type="button" className='btn btn-outline-light btn-5'>Сообщения</button></a>
-                    <a href='/music'><button type="button" className='btn btn-outline-light btn-3'>Музыка</button></a>
-                    <a href='/videos'><button type="button" className='btn btn-outline-light btn-4'>Видеозаписи</button></a>
-                    <a href='/settings'><button type="button" className='btn btn-outline-light btn-4'>Настройки</button></a>
+                    <NavLink to='/feed'><button type="button" className='btn btn-outline-light btn-1'>Новости</button></NavLink>
+                    <NavLink to='/profile'><button type="button" className='btn btn-outline-light btn-2'>Профиль</button></NavLink>
+                    <NavLink to='/friends'><button type="button" className='btn btn-outline-light btn-6'>Друзья</button></NavLink>
+                    <NavLink to='/messages'><button type="button" className='btn btn-outline-light btn-5'>Сообщения</button></NavLink>
+                    <NavLink to='/music'><button type="button" className='btn btn-outline-light btn-3'>Музыка</button></NavLink>
+                    <NavLink to='/videos'><button type="button" className='btn btn-outline-light btn-4'>Видеозаписи</button></NavLink>
+                    <NavLink to='/settings'><button type="button" className='btn btn-outline-light btn-4'>Настройки</button></NavLink>
                 </nav>
             </div>
         )
